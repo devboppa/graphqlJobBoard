66c33698d9503531cd994395f55e196a3df6a349
rajuboppa <rajupatelboppa@gmail.com>
2025-01-07 13:51:54 -0500

    adding some more functionality and resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,21 @@
 import { getJobs } from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
 export const resolvers = {
   Query: {
     greeting: () => "Hello world!",
     jobs: async () => getJobs(),
   },
+
+  Job: {
+    date: (job) => {
+      return toIsoDate(job.createdAt);
+    },
+    company: (job) => {
+      return getCompany(job.companyId);
+    },
+  },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
