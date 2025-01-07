bb38e5e98f61fb374e8ed1626213865388ece9e9
rajuboppa <rajupatelboppa@gmail.com>
2025-01-07 13:22:19 -0500

    connecting to a db and passing actual data with our api

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,19 +1,7 @@
+import { getJobs } from "./db/jobs.js";
 export const resolvers = {
   Query: {
     greeting: () => "Hello world!",
-    jobs: () => {
-      return [
-        {
-          id: "test-id-1",
-          title: "Software Engineer",
-          description: "Develop software projects with professionalism.",
-        },
-        {
-          id: "test-id-2",
-          title: "Software Engineer",
-          description: "Develop software projects with professionalism.",
-        },
-      ];
-    },
+    jobs: async () => getJobs(),
   },
 };
